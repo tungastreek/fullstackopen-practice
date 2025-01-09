63172a70733d7d0e8fa2f3517b0fb66906633f14
tungastreek <tung@astreek.com>
2025-01-10 00:12:47 +0700

    WIP

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,25 +1,60 @@
-const Hello = (props) => {
-  console.log(props);
+import { useState } from "react"
+
+const App = () => {
+
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [clickHistory, setClickHistory] = useState([])
+  const [totalCLicks, setTotalClicks] = useState(0)
+
+  const handleLeftClick = () => {
+    setLeft(left + 1)
+    setClickHistory(clickHistory.concat('L'))
+    // Total will lag behind one value
+    // Because the component will only rerendered after all code inside the handler executed
+    // So left inside here will stay the same for each call
+    setTotalClicks(left + right)
+  }
+
+  const handleRightClick = () => {
+    setRight(right + 1)
+    setClickHistory(clickHistory.concat('R'))
+    // Total will lag behind one value
+    // Because the component will only rerendered after all code inside the handler executed
+    // So left inside here will stay the same for each call
+    setTotalClicks(left + right)
+  }
+
   return (
-    <p>Hello World from {props.name}. I am {props.age} years old.</p>
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text='Left' />
+      <Button onClick={handleRightClick} text='Right' />
+      {right}
+      <History clickHistory={clickHistory} />
+      <p>Total clicks(lagging one): {totalCLicks}</p>
+    </div>
   )
 }
 
-const App = () => {
-  const name = "Lysa";
-  const age = 0;
-  const now = new Date();
-  const friends = ["Sam", "Lysa"];
-  return (
-    <div>
-      <Hello name="Lic" age={36} />
-      <Hello name="Ly" age={36} />
-      <Hello name="Sam" age={4} />
-      <Hello name={name} age={age} />
-      <p>Hello world. The time is {now.toString()}.</p>
-      <p>{friends}</p>
-    </div>
-  )
+const Button = ({ onClick, text }) => (
+  <button type="button" onClick={onClick}>{text}</button>
+)
+
+const History = ({ clickHistory }) => {
+  if (clickHistory.length === 0) {
+    return (
+      <div>
+        The app is use by pressing the buttons
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        Buttons press hitory: {clickHistory.join(' ')}
+      </div>
+    )
+  }
 }
 
 export default App
