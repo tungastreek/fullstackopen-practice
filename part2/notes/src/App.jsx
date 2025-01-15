6b517a49c4addc2cafe5fc3fae1c2bf1c9c2ebe9
tungastreek <tung@astreek.com>
2025-01-15 11:15:19 +0700

    WIP practice part 2

---
+++ part2/notes/src/App.jsx
@@ -0,0 +1,17 @@
+import Note from './components/Note'
+
+const App = (props) => {
+  const { notes } = props
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note =>
+          <Note key={note.id} content={note.content} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
