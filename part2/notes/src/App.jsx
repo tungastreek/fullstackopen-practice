cfd46a0a8aa685d3bc4847ca27638f7a74a58d61
tungastreek <tung@astreek.com>
2025-01-16 13:38:51 +0700

    WIP Part 2

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,15 +1,48 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
 const App = (props) => {
-  const { notes } = props
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const handleNoteChange = (e) => {
+    setNewNote(e.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  const addNote = (e) => {
+    e.preventDefault()
+    const newNoteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    }
+    setNotes(notes.concat(newNoteObject))
+    setNewNote('')
+  }
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? 'Important' : 'All'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note =>
+        {notesToShow.map(note =>
           <Note key={note.id} content={note.content} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <br />
+        <button type="submit">Save</button>
+      </form>
     </div>
   )
 }
